93fd7c6de6c9b12fae998291fdccd2a3bb3c8583
Centrilogic_Chunggil <cghan@centrilogic.com>
2020-05-31 13:39:07 -0400

    First commit

---
+++ src/build/precache-manifest.b8719ed1e6ff6c278193050663bb032f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b934e158488138a987801196190f8de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "975962bb8ed9208416e8",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "c2be464aa755dd558ed8",
+    "url": "/static/js/2.a38ab3ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a38ab3ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975962bb8ed9208416e8",
+    "url": "/static/js/main.8adc4d3b.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
